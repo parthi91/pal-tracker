f1ea54778730b483af5a34b00ddcdccd14fc812c
parthi91 <parthiban.thangaraj@perficient.com>
2018-12-05 14:02:24 +0530

    app properties added

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,24 +1,23 @@
 package io.pivotal.pal.tracker;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class WelcomeController {
-    private String msg;
-    @GetMapping("/")
-    @ResponseBody
-    public String sayHello() {
-        return msg;
-    }
-    public WelcomeController(@Value("${WELCOME_MESSAGE}") String msg) {
-         //public WelcomeController(String msg) {
-        this.msg=msg;
-    }
 
-    public WelcomeController() {
-        this("Hello from test");
-    }
-}
+        private String message;
+
+
+        public WelcomeController( @Value("${WELCOME_MESSAGE}")  String msg){
+            message=msg;
+        }
+
+        @GetMapping("/")
+    public String sayHello() {
+            return message;
+        }
+        }
+
